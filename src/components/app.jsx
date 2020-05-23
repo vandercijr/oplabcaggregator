3a64d5b9d04fb9a557dd0a5bf7276b74c2563bd9
Vanderci <vandercijr@gmail.com>
2020-05-23 18:28:43 -0300

    add menu component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,31 +2,12 @@ import React from 'react';
 import IOSortRunner from './iosortrunner';
 import IOLinkedListRunner from './iolinkedlistrunner';
 import IOGameRunner from './iogamerunner';
+import Menu from './menu';
 
 class App extends React.Component {
   render() {
     return  <React.Fragment>
-              <nav className="navbar fixed-top navbar-light bg-light">
-                <div className="container">
-                  <ul className="nav">
-                    <li className="nav-item">
-                      <a className="nav-link active" href="#bubble">Bubble Sort</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#linkedlist">Linked List</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#gameofstones">Game Of Stones</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link disabled" href="#twitter">Twitter</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="http://localhost:42399" target="noopener noreferrer">Template</a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
+              <Menu />
               <div className="row iorunner">
                 <div className="col" id="bubble">
                   <IOSortRunner title="Bubble Sort" runText="Order"/>
