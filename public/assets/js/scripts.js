1d005c6feacdf055d54a85fae24230fcf22f4f99
Vanderci <vandercijr@gmail.com>
2020-05-23 13:39:56 -0300

    add index.html and assets

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(()  =>  {
+//   $("nav a").click(()  =>  {
+//     e.preventDefault();
+//     console.log($(this).attr(href));
+// console.log(e.getAttribute("href"));
+//     document.getElementById("linkedlist").scrollIntoView();
+//   });
+});
